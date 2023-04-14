50f678f6209b6d8b3cf39b88e1c1e44d9343d3cb
SrinjoyMaity <rawnofficial005@gmail.com>
2023-04-15 00:05:54 +0530

    commit

--- src/Main.js
+++ src/Main.js
@@ -2,17 +2,18 @@ import { useState , useEffect } from "react";
 import {useCookies} from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import pp from "./images/profilepic.jpg";
+import LowbarSelect from "./LowbarSelect.js";
 import './main.css';
 
-function Main(props)
+ function Main(props)
 {
     const [img, setImg]=useState();
-    const[ accdata,setAccdata]=useState();
+    const[ lowbar,setLowbar]=useState(0);
     const [dispName, setDispName] =useState("");
     const [cookies, setCookie,removeCookie]=useCookies(['enquiryUser']);
+    const [id, setId]=useState();
     const navigate=useNavigate();
 
-    var data;
     useEffect(()=>{
         if(cookies.enquiryUser=== undefined)
         {
@@ -33,8 +34,8 @@ function Main(props)
                     {
                         res.json().then(function(res){
                             console.log(res);
-                            data=res;
                             setDispName(res.firstname+" "+res.lastname+" "+res.roll);
+                            setId(res._id);
                             if(res.dp===null)
                             {
                                 setImg(pp);
@@ -68,6 +69,26 @@ function Main(props)
         removeCookie("enquiryUser");
         navigate('/');
     }
+    function handleEdit()
+    {
+        setLowbar(1);
+    }
+    function handleCabpool()
+    {
+        setLowbar(2);
+    }
+    function handleLostfound()
+    {
+        setLowbar(3);
+    }
+    function handleAdmin()
+    {
+        setLowbar(4);
+    }
+    function handleDeactivate()
+    {
+        setLowbar(5);
+    }
     if(cookies.enquiryUser=== undefined)
     {
             navigate("/");
@@ -77,18 +98,19 @@ function Main(props)
             <div className="topbar">
             <img src={img} className="profilepic" alt="profilepic" />
             <h1 className="displayname">{dispName}</h1>
-            <button className = "accedit">Edit</button>
+            <button className = "accedit" onClick={handleEdit}>Edit</button>
                 <button className="logout" onClick={handlelogout}>log out</button>
-                <button className="deactivate">deactivate</button>
-                <button id="admin" onClick={handlelogout}>admin</button>
+                <button className="deactivate" onClick={handleDeactivate}>deactivate  </button>
+                <button id="admin" onClick={handleAdmin}>admin</button>
             </div>
             <div className="midbar">
-                <button className="cabpool">Cab-pool dashboard</button>
-                <button className="lostfound">lost/found enquiry dashboard</button>
+                <button className="cabpool" onClick={handleCabpool}>Cab-pool dashboard</button>
+                <button className="lostfound" onClick={handleLostfound}>lost/found enquiry dashboard</button>
             </div>
-            <div className="lowbar"></div>
-        </div>
-        
+            <div className="lowbar">
+               <LowbarSelect sel={lowbar}/>
+            </div>
+        </div> 
     )
 }
 
